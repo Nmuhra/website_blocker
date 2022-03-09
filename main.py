6b9b187a1d709404dcdaaef9bb793b69b8cb52ea
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-03-09 21:58:26 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+path = input("host path: \n")
+host_path="path"
+redirect = "127.0.0.1"
+
+website_list=["www.facebook.com","www.instagram.com"]
+
+start_date = datetime(2022,3,9)
+end_date = datetime(2022,3,10)
+today_date = datetime(datetime.now().year,datetime.now().month,datetime.now().day)
